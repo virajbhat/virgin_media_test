f2ff3e6043e80323046acec1a4669598488420a4
virajbhat <virajbhat69@gmail.com>
2022-08-21 17:23:15 +0100

    generated input

---
+++ src/main.py
@@ -0,0 +1,31 @@
+from google.cloud import bigquery
+
+
+import os
+#os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="C:\\virgin_media_test\\virgin_media_test\\keys\\ingo-232414-1da4eed6fb70.json"
+# Construct a BigQuery client object.
+client = bigquery.Client()
+
+# TODO(developer): Set table_id to the ID of the table to create.
+# table_id = "your-project.your_dataset.your_table_name"
+
+job_config = bigquery.LoadJobConfig(
+    schema=[
+        bigquery.SchemaField("name", "STRING"),
+        bigquery.SchemaField("post_abbr", "STRING"),
+    ],
+    skip_leading_rows=1,
+    # The source format defaults to CSV, so the line below is optional.
+    source_format=bigquery.SourceFormat.CSV,
+)
+uri = "gs://cloud-samples-data/bigquery/sample-transactions/transactions.csv"
+table_id = 'transactions.csv'
+
+load_job = client.load_table_from_uri(
+    uri, table_id, job_config=job_config
+)  # Make an API request.
+
+load_job.result()  # Waits for the job to complete.
+
+destination_table = client.get_table(table_id)  # Make an API request.
+print("Loaded {} rows.".format(destination_table.num_rows))
